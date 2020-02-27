b8e3ea280b2aa2e5e721ad8b7a806a8f8455f801
shirwet Elfeel <eshirwet@yahoo.com>
2020-02-27 01:47:54 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -43,29 +43,19 @@ def naming_system
   # HoH all the way to Species!
 
 hash = {			
-			label: "Kingdom",
-			sub_category: {
-			label: "Phylum",
-			sub_category: {
-			label: "Class"
-			}
-			}
-			},
-			
-			 {
-			label: "Order"
-			},
-			
-			{
-			label: "Family",
-			sub_category: {
-			label: "Genus",
-			}
-			},
-			
-			{
-			label: "Species",
-			sub_category: nil
-			}
+		label: "Kingdom",
+sub_category: {
+label: "Phylum",
+sub_category: {
+label: "Class",
+sub_category: {
+label: "Order",
+sub_category: {
+label: "Family",
+sub_category: {
+label: "Genus",
+sub_category: {
+label: "Species",
+sub_category: nil } } } } } } }
 			
 end
